6fc63ef5bc7e068bcf661e300e2000b95506bfd7
Moonlight <moonlight9512@gmail.com>
2025-11-02 15:55:47 +0500

    Add tests for the API

--- src/test/java/gismp/HttpTest.java
+++ src/test/java/gismp/HttpTest.java
@@ -6,9 +6,13 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import gismp.CrptApi.RequestDocument;
 
 /**
  * This test makes sure the HttpClient
@@ -42,11 +46,29 @@ public class HttpTest {
 					);
 			statusCode = httpResponse.statusCode();
 		} catch (IOException | InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.err.printf("Exception happened: %s%n", e.getLocalizedMessage());
 		}
 		
 		Assertions.assertEquals(200, statusCode);
+	}
+	
+	@Test
+	public void accessDenied() {
+		CrptApi api = new CrptApi(TimeUnit.SECONDS, 1);
+		HttpResponse<String> response = null;
+		try {
+			response = api.createDocument(
+					new RequestDocument(), "no-signature"
+					).get();
+			int statusCode = response.statusCode();
+			Assertions.assertEquals(403, statusCode);
+			Assertions.assertEquals(
+					"Billing contract not signed by participant.",
+					response.body()
+					);
+		} catch (InterruptedException | ExecutionException e) {
+			System.err.printf("Exception happened: %s%n", e.getLocalizedMessage());
+		}
 	}
 	
 }
